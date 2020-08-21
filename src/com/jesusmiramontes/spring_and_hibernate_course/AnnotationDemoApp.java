9841264ee57b9f3b45f01859e3599d19713acb98
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2020-08-20 23:19:33 -0500

    Create a bean using annotations

---
+++ src/com/jesusmiramontes/spring_and_hibernate_course/AnnotationDemoApp.java
@@ -0,0 +1,22 @@
+package com.jesusmiramontes.spring_and_hibernate_course;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+    public static void main(String[] args) {
+
+        // read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get the bean from spring container
+        // The name must be the same as defined at TennisCoach.java in the component annotation.
+        Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+
+        // call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the context
+        context.close();
+
+    }
+}
--- src/com/jesusmiramontes/spring_and_hibernate_course/TennisCoach.java
+++ src/com/jesusmiramontes/spring_and_hibernate_course/TennisCoach.java
@@ -2,6 +2,8 @@ package com.jesusmiramontes.spring_and_hibernate_course;
 
 import org.springframework.stereotype.Component;
 
+//Spring will scan the files and register this bean automatically
+
 @Component("thatSillyCoach")
 public class TennisCoach implements Coach{
     @Override
