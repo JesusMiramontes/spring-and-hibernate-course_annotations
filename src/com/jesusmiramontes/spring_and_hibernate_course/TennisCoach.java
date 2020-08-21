85f90f663cb8b08808192d157a7ab981a1f3c2ee
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2020-08-20 23:13:52 -0500

    Add @component annotation

--- src/com/jesusmiramontes/spring_and_hibernate_course/TennisCoach.java
+++ src/com/jesusmiramontes/spring_and_hibernate_course/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.jesusmiramontes.spring_and_hibernate_course;
 
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
 public class TennisCoach implements Coach{
     @Override
     public String getDailyWorkout() {
