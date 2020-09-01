e3ced834af77e972869f0e8a0723d4d1b84ab3f1
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2020-09-01 00:58:10 -0500

    Spring config file with dependency injection through constructor

--- src/com/jesusmiramontes/spring_and_hibernate_course/SportConfig.java
+++ src/com/jesusmiramontes/spring_and_hibernate_course/SportConfig.java
@@ -1,10 +1,23 @@
 package com.jesusmiramontes.spring_and_hibernate_course;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // This indicates this is a configuration file
-@ComponentScan("com.jesusmiramontes.spring_and_hibernate_course") // This indicates which packages classes will be scanned for annotations
+//@ComponentScan("com.jesusmiramontes.spring_and_hibernate_course") // This indicates which packages classes will be scanned for annotations
 public class SportConfig {
+
+    // define bean for sad fortune service
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
+
+    // define bean for swim coach and inject dependency
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach(sadFortuneService());
+    }
 
 }
