e37d9d9423661da33de40d1f51a4ca1be0c63ce4
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2020-09-01 00:31:30 -0500

    Spring configuration using annotations and no xml

---
+++ src/com/jesusmiramontes/spring_and_hibernate_course/JavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.jesusmiramontes.spring_and_hibernate_course;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+    public static void main(String[] args) {
+        // load spring file
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        // Check if they are the same
+        boolean result = (theCoach == alphaCoach);
+
+        // print result
+        System.out.println("\n Pointing to the same object: " + result);
+        System.out.println("\n Memory location for theCoach" + theCoach);
+        System.out.println("\n Memory location for alphaCoach" + alphaCoach);
+
+        context.close();
+    }
+}
---
+++ src/com/jesusmiramontes/spring_and_hibernate_course/SportConfig.java
@@ -0,0 +1,10 @@
+package com.jesusmiramontes.spring_and_hibernate_course;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // This indicates this is a configuration file
+@ComponentScan("com.jesusmiramontes.spring_and_hibernate_course") // This indicates which packages classes will be scanned for annotations
+public class SportConfig {
+
+}
